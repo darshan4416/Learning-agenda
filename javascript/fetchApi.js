59a763c60814074c500f39009fdf7ab34e787531
darshan4416 <darshanhirekurubar4416@gmail.com>
2022-01-20 00:23:55 +0530

    js added

--- javascript/fetchApi.js
+++ javascript/fetchApi.js
@@ -1,1 +1,11 @@
 
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then((data)=>{
+    return data.json();
+})
+.then((actualData)=>{
+    console.log(actualData);
+})
+.catch((error)=>{
+    console.log(error);
+})
